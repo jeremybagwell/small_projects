58dc540670b0f775d2b4d906a583fd8c827c2bbb
Jeremy Bagwell <jeremy.r.bagwell@medtroinic.com>
2023-06-20 02:53:15 +0000

    working test lib with mnavlogging

--- test_lib.cpp
+++ test_lib.cpp
@@ -1,8 +1,48 @@
-#include <iostream>
-#include "mylibrary2.h"
+#include "MNavLog.h"
+// #include <iostream> // only for dev, printing to console
 
-int main() {
-    MyLibrary myLibrary;
-    myLibrary.sayHello();
+using namespace MNavLogger;
+
+static MNavLogger::MNavLogger logger("AboutThisStealthService");
+
+int main()
+{
+    //Security Logging Functions
+    logger.secLow("5654sd55", SecType::AUTZ, SecStatus::PASS, "C++/Security/Info Message");
+    // logger.secMed("978D875D", SecType::AUTN, SecStatus::FAIL, "C++/Security/Warn Message");      
+    // logger.secHigh("978D875D", SecType::AUTN, SecStatus::FAIL, "C++/Security/Error Message");      
+    // // logger.secCrit("978D875D", SecType::ADMN, SecStatus::FAIL, "C++/Security/Fatal Message"); //Often commented out because Fatal log4cxx events sent to rsyslog are "emergency" and printed to all terminals          
+
+    // //Diagnostic Logging Macros
+    // DIAG_TRACE(logger, "521585k5", "C++/Diagnostic/Info Message [from MACRO]");
+    // DIAG_DEBUG(logger, "521585k5", "C++/Diagnostic/Info Message [from MACRO]");
+    // DIAG_INFO(logger, "521585k5", "C++/Diagnostic/Info Message [from MACRO]");
+    // DIAG_WARN(logger, "521585k5", "C++/Diagnostic/Info Message [from MACRO]");
+    // DIAG_ERROR(logger, "521585k5", "C++/Diagnostic/Info Message [from MACRO]");
+    // // DIAG_CRIT(logger, "521585k5", "C++/Diagnostic/Info Message [from MACRO]"); //Often commented out because Fatal log4cxx events sent to rsyslog are "emergency" and printed to all terminals     
+
+    // //Diagnostic Logging Functions
+    // if (logger.isEnabledTrace()){
+    //     logger.diagTrace("5654sd55", "C++/Diagnostic/Trace Message");
+    // }
+    // if (logger.isEnabledDebug()){
+    //     logger.diagDebug("5654sd55", "C++/Diagnostic/Debug Message");
+    // }
+    // if (logger.isEnabledInfo()){
+    //     logger.diagInfo("5654sd55", "C++/Diagnostic/Info Message");
+    // }
+    // logger.diagWarn("978D875D", "C++/Diagnostic/Warn Message");      
+    // logger.diagError("978D875D", "C++/Diagnostic/Error Message");      
+    // // logger.diagCrit("978D875D", "C++/Diagnostic/Fatal Message")); //Often commented out because Fatal log4cxx events sent to rsyslog are "emergency" and printed to all terminals     
+
+    // //Test when switching the ID and the message
+    // DIAG_INFO(logger, "C++/Diagnostic/Info Message [from MACRO] with CID and message mistakenly swapped", "521585k5");
+
+    // //Test using the left shift operator << to form the message
+    // DIAG_INFO(logger, "521585k5", "C++/Diagnostic/Info Message" << " Formed using the left shift operator");
+    // logger.diagInfo("5654sd55", MNAV_SS("C++/Diagnostic/Fatal Message " << 5 << " Formed using the left shift operator"));
+
+
+
     return 0;
 }
--- test_lib.sh
+++ test_lib.sh
@@ -1,3 +1,5 @@
-g++ test_lib.cpp -o test_lib -lmnavlogging
+export MNAV_STEALTHAPP_LOG_ROOT="../mnav-logging/logging/config"
+
+g++ -std=c++20 test_lib.cpp -o test_lib -lmnavlogging -llog4cxx -lapr-1 -laprutil-1 
 ./test_lib
 
